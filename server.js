5bd2e995dae6825204e70eae3434cd12bc7215fe
Abdallah Yassein <abdallah.work.1998@gmail.com>
2025-01-13 18:26:21 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve static files from the .well-known folder
+app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
+
+// Handle /product/:id route
+app.get('/product/:id', (req, res) => {
+    const productId = req.params.id;
+    res.send(`Product ID: ${productId}`);
+});
+
+// Default route
+app.get('/', (req, res) => {
+    res.send('Deep Link Server is running!');
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
