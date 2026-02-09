a4c9e715fe06a5914c0bf34d23a32bdc7e202e0f
Thurein Cloud <thureinh64@gmail.com>
2026-02-09 22:50:05 +0630

    Deep Seeking for mobile apps

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Serve static files from the .well-known folder
 app.use('/.well-known', express.static(path.join(__dirname, '.well-known')));
